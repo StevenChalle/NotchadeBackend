8109974d7d85ee2fe595cda265d145693174c8e9
Steven Challe <stchalle@gmail.com>
2018-11-13 14:54:43 +0100

    finished AddUser

--- routes/users/add.js
+++ routes/users/add.js
@@ -4,18 +4,30 @@ module.exports = {
     addUser: (req, res, next) => {
       console.log("route addUser called")
 
-      try {
-        let query = `insert into users (pseudo, email, password) values ('${req.body.pseudo}', '${req.body.email}', '${req.body.password}')`
-        mysqlClient.query(query, (err, result) => {
-          if (err) throw err
+      //does user credentials already exist ?
+      let query = `select * from users
+      where pseudo = '${req.body.pseudo}' or email = '${req.body.email}'`
+      mysqlClient.query(query, (err, result) => {
+        if (err) throw err
+        if (result.length > 0) {
           res.send(JSON.stringify({
-            "status": 200,
-            "error": null,
-            "response": `done`
+            "status": 409,
+            "error": "Conflict",
+            "response": `Pseudo or email address already exists`
           }))
-        })
-      } catch (e) {
-        console.log(e)
-      }
+        }
+        else {
+          query = `insert into users (pseudo, email, password)
+          values ('${req.body.pseudo}', '${req.body.email}', '${req.body.password}')`
+          mysqlClient.query(query, (err, result) => {
+            if (err) throw err
+            res.send(JSON.stringify({
+              "status": 200,
+              "error": null,
+              "response": `done`
+            }))
+          })
+        }
+      })
     }
 }
